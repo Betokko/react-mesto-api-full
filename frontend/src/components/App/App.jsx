5b10fdbe15dd270131e532b2056b70addeb08769
artur nikitin <branewor@gmail.com>
2022-07-05 11:31:49 +0400

    fix

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,24 +1,24 @@
-import { useState, useEffect } from 'react';
-import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
-import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import { useState, useEffect } from "react";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-import api from '../../utils/API';
-import * as auth from '../../utils/auth';
+import api from "../../utils/API";
+import * as auth from "../../utils/auth";
 
-import Header from '../Header/Header';
-import Main from '../Main/Main';
-import Footer from '../Footer/Footer';
-import PopupWithForm from '../PopupWithForm/PopupWithForm';
-import ImagePopup from '../ImagePopup/ImagePopup';
-import EditProfilePopup from '../EditProfilePopup/EditProfilePopup';
-import EditAvatarPopup from '../EditAvatarPopup/EditAvatarPopup';
-import AddPlacePopup from '../AddPlacePopup/AddPlacePopup';
-import Login from '../Login/Login';
-import Register from '../Register/Register';
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
-import InfoTooltip from '../InfoTooltip/InfoTooltip';
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+import PopupWithForm from "../PopupWithForm/PopupWithForm";
+import ImagePopup from "../ImagePopup/ImagePopup";
+import EditProfilePopup from "../EditProfilePopup/EditProfilePopup";
+import EditAvatarPopup from "../EditAvatarPopup/EditAvatarPopup";
+import AddPlacePopup from "../AddPlacePopup/AddPlacePopup";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import InfoTooltip from "../InfoTooltip/InfoTooltip";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
@@ -28,13 +28,13 @@ function App() {
   const [selectedCard, setSelectedCard] = useState(null);
   const [currentUser, setCurrentUser] = useState({});
   const [cards, setCards] = useState([]);
-  const [linkName, setLinkName] = useState('');
-  const [linkUrl, setLinkUrl] = useState('');
+  const [linkName, setLinkName] = useState("");
+  const [linkUrl, setLinkUrl] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
   const [userData, setUserData] = useState({});
   const [isTooltipPopupOpen, setIsTooltipPopupOpen] = useState(false);
   const [tooltipPopupStatus, setTooltipPopupStatus] = useState(false);
-  const [tooltipPopupMessage, setTooltipPopupMessage] = useState('');
+  const [tooltipPopupMessage, setTooltipPopupMessage] = useState("");
 
   const history = useHistory();
 
@@ -81,7 +81,7 @@ function App() {
     api
       .removeCard(card._id)
       .then(() => {
-        setCards((state) => state.filter((c) => (c._id !== card._id ? c : '')));
+        setCards((state) => state.filter((c) => (c._id !== card._id ? c : "")));
       })
       .catch((err) => console.log(err));
   }
@@ -89,13 +89,13 @@ function App() {
   function handleAddPlaceSubmit(newCard) {
     api
       .loadCard(newCard)
-      .then(() => {
+      .then((res) => {
         newCard.owner = currentUser._id;
         newCard.likes = [];
+        newCard._id = res._id;
       })
       .then(() => setCards([newCard, ...cards]))
       .then(() => closeAllPopups())
-      .then(() => window.location.reload())
       .catch((err) => console.log(err));
   }
 
@@ -170,13 +170,13 @@ function App() {
   }
 
   function checkToken() {
-    const JWT = localStorage.getItem('JWT');
+    const JWT = localStorage.getItem("JWT");
     if (JWT) {
       auth
         .checkValidityToken(JWT)
         .then((res) => {
           setLoggedIn(true);
-          history.push('/');
+          history.push("/");
           setUserData({ _id: res._id, email: res.email });
           getProfileInfo();
           getInitialCards();
@@ -191,7 +191,8 @@ function App() {
       .then((res) => {
         setIsTooltipPopupOpen(true);
         setTooltipPopupStatus(true);
-        history.push('/sign-in');
+        setTooltipPopupMessage("Это успех!");
+        history.push("/sign-in");
       })
       .catch((err) => {
         setIsTooltipPopupOpen(true);
@@ -204,9 +205,9 @@ function App() {
     auth
       .login(userData)
       .then((res) => {
-        localStorage.setItem('JWT', res.token);
+        localStorage.setItem("JWT", res.token);
         setLoggedIn(true);
-        history.push('/');
+        history.push("/");
         window.location.reload();
         checkToken();
       })
